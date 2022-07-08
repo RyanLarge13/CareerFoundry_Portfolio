064b0c5af0c32244c7fbd47af3f82eacd0d34297
Ryan Large <101679941+RyanLarge13@users.noreply.github.com>
2022-07-08 13:32:55 -0700

    Implementing live meai;l and telephone number validation on contact form
    also I adjusted some padding on the about section for mobile devices

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+setTimeout(() => {
+    let emailForm = document.querySelector('#email');
+    let telForm = document.querySelector('#tel');
+    emailForm.addEventListener('keydown', (e) => {
+        let value = emailForm.value;
+        let error = document.querySelector('.error-message-email');
+        let regex = /@/;
+        if (regex.test(value)) {
+            error.style.display = 'none';
+        } else {
+            error.style.display = 'block';
+        }
+    });
+    telForm.addEventListener('keydown', () => {
+        let value = telForm.value;
+        let error = document.querySelector('.error-message-tel');
+        let regex = /\d/gi;
+        if (regex.test(value)) {
+            error.style.display = 'none';
+        } else {
+            error.style.display = 'block';
+        }
+    });
+}, 1);
